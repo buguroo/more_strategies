4c3b22adea66f90389d284acf4831023ee7c707b
Samuel Herrero <sherrero@buguroo.com>
2015-06-09 16:44:27 +0200

    Strategy `IP` renamed to `ip`.

--- more_strategies/ip.py
+++ more_strategies/ip.py
@@ -8,6 +8,6 @@ def to_ip(un32):
     return ipaddress.ip_address(un32)
 
 
-def IP():
+def ip():
     ipst = strategy(st.integers(min_value=0, max_value=int(0xffffffff)))
     return ipst.map(to_ip)
